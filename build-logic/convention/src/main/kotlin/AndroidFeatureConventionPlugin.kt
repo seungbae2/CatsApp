3e9a84891fe5c57fa267cceda177434f9c3be319
seungbae2 <seungbae2@gmail.com>
2025-04-30 17:19:36 +0900

    feat: 앱 네비게이션 구성
    - MainNavHost -> CatsScreen 네비게이션 구현

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -14,6 +14,7 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
             }
 
             dependencies {
+                "implementation"(project(":core:ui"))
                 "implementation"(libs.findLibrary("androidx.hilt.navigation.compose").get())
                 "implementation"(libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
                 "implementation"(libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
--- feature/cats/src/main/java/com/example/catsapp/feature/cats/navigation/CatsNavigation.kt
+++ feature/cats/src/main/java/com/example/catsapp/feature/cats/navigation/CatsNavigation.kt
@@ -1,2 +1,20 @@
 package com.example.catsapp.feature.cats.navigation
 
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.example.catsapp.feature.cats.CatsRoute
+import kotlinx.serialization.Serializable
+
+@Serializable
+data object CatsRoute
+
+fun NavController.navigateToCats(navOptions: NavOptions) =
+    navigate(route = CatsRoute, navOptions)
+
+fun NavGraphBuilder.catsScreen() {
+    composable<CatsRoute> {
+        CatsRoute()
+    }
+}
